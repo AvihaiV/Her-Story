fdd6c60cc419468f4b34a337fad8b9e25424209d
AvihaiV <avihai673@gmail.com>
2017-03-21 11:40:43 +0200

    creating main page

--- Yerushalmi-App/src/app/app.module.ts
+++ Yerushalmi-App/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './login/login/login.component';
+import { HeaderComponent } from './structure/header/header.component';
+import { FooterComponent } from './structure/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
